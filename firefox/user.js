e9c027b0314b604d647e82b955ff318b0feb0fbe
MBK <marbuka@protonmail.com>
2023-07-05 12:17:44 +0200

    added user.js tweaks

---
+++ firefox/user.js
@@ -0,0 +1,6 @@
+// restore compact mode
+user_pref("browser.compactmode.show", true);
+// enable styling through CSS
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// don't fold menu list when a bookmark is clicked
+user_pref("browser.bookmarks.openInTabClosesMenu", false);
