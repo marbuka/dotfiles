5cc3d52b85553d450ac687bbcd0fdf0f5b85db0c
MBK <marbuka@proton.me>
2024-01-28 15:30:39 +0100

    disable Pocket

--- firefox/user.js
+++ firefox/user.js
@@ -4,3 +4,5 @@ user_pref("browser.compactmode.show", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // don't fold menu list when a bookmark is clicked
 user_pref("browser.bookmarks.openInTabClosesMenu", false);
+//disable Pocket
+user_pref("extensions.pocket.enabled", false);
